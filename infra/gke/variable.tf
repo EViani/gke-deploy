5a5463d36c662a7c1cda5da89b4f52a1d1fd4824
EstuViani <eviani94@hotmail.com>
2026-07-02 19:12:00 -0300

    Delete bucket variable

--- infra/gke/variable.tf
+++ infra/gke/variable.tf
@@ -2,8 +2,3 @@ variable "project_id" {
   type        = string
   description = "El ID del proyecto de GCP provisto por GitHub Actions"
 }
-
-variable "bucket_gcp" {
-  description = "Name of bucket in GCP"
-  type        = string
-}
